fa179a1da6ffe76e5c35de1aa9991a926d7eba49
Jonathan Ramirez <jonathan.ramirez@me.com>
2018-05-01 20:16:15 -0300

    v1 para Revision

---
+++ README.md
@@ -0,0 +1,11 @@
+# METODOLOGÍAS DE DISEÑO E IMPLANTACIÓN
+
+Autor: Jonathan Ramírez - Universidad Santa Maria - Chile
+
+### EXPLICACIÓN
+
+La presente tarea esta consumiendo un JSON público del **Ministerio de Salud** de Chile y que es muy conocido y utilizado llamado [Farmacia de Turno](http://datos.gob.cl/dataset/farmacias-en-chile/resource/41e27832-83f4-47c8-9bec-6585605a1098), este servicio se obtiene los listados de farmacias a través del territorio nacional obtenido de Farmanet (MINSAL). La idea es demostrar la utilización JSON y la manipulación del DOM mostrando los resultados en una simple tabla HTML.
+
+Estoy ocupando **Bootstrap 3** para hacer más fácil el **Front-End** y utilizando la libreria **jQuery** para las llamadas **Ajax**.
+
+Finalmente mencionar que por mótivos prácticos la búsqueda solamente se restringe a la **Octava Región** y para el día **01/05/2018**.
---
+++ js/init.js
@@ -0,0 +1,33 @@
+$(function() {
+	// variables globales
+	var dateToday = new Date();
+	var tableRow = '';
+	var tableDiv = $('#tableDiv');
+	// mostrar la fecha de hoy
+	$('strong.dateToday').html(dateToday.getDate() + '/' + (dateToday.getMonth()+1) + '/' + dateToday.getFullYear())
+	// get ajax farmacias de turno
+	$.ajax({
+		url: 'js/farmacias01052018.json',
+		type: 'GET',
+		dataType: 'json'
+	}).done(function(data, textStatus, errorThrown) {
+		if(textStatus === 'success'){
+			// recorrer todo los datos para mostrarlos en la tabla
+			$.each(data, function(index, row) {
+				// crear html con fila
+				tableRow += '<tr>';
+				tableRow += '<td>'+row.local_nombre+'</td>';
+				tableRow += '<td>'+row.local_direccion+'</td>';
+				tableRow += '<td>'+row.localidad_nombre+'</td>';
+				tableRow += '<td>'+row.funcionamiento_hora_apertura+' PM</td>';
+				tableRow += '<td>'+row.funcionamiento_hora_cierre+' AM</td>';
+				tableRow += '<td>'+row.local_telefono+'</td>';
+				tableRow += '<td><a href="https://www.google.com/maps/search/'+row.local_lat+','+row.local_lng+'" target="_blank" title="Ver Ubicación de la Farmacia en Google Maps">Ver Mapa</a></td>';
+				tableRow += '</tr>';
+			});
+			// agregar al dom la informacion de la farmacia
+			tableDiv.find('tbody').append(tableRow);
+		}
+	}).fail(function(response, textStatus, errorThrown) {
+	});
+});
